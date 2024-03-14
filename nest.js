fa53aa3ad2ff07e9f20c9a4b15a3b1d6f16b5732
Keenest Callas <admin@elsikora.com>
2024-03-14 22:00:45 +0300

    Simplify eslint configuration in nest.js
    The eslint configuration settings in nest.js have been streamlined for readability and efficiency. Unnecessary setting layers have been removed without changing environmental and rule extension settings, thus maintaining the same function but with a more succinct and easy-to-read structure.

--- nest.js
+++ nest.js
@@ -1,9 +1,7 @@
 module.exports = {
-    env: {
-        es2021: true,
-        node: true,
-    },
-    extends: [
-        "plugin:@elsikora/nestjs-typed/recommended"
-    ],
+	env: {
+		es2021: true,
+		node: true,
+	},
+	extends: ["plugin:@elsikora/nestjs-typed/recommended"],
 };
